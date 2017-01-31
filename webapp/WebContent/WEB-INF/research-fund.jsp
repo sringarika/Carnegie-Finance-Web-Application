e954e292e16788ddf5253b4956da2b32f1457bb3
Yuchen Shi <yuchenshi@cmu.edu>
2017-01-30 23:44:55 -0500

    Reformat research-fund.jsp.

--- webapp/WebContent/WEB-INF/research-fund.jsp
+++ webapp/WebContent/WEB-INF/research-fund.jsp
@@ -1,47 +1,53 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ include file="header.jsp" %>
 <main>
     <h2>Research Funds</h2>
 <div>
 </div>
+    <h3>Latest Closing Price</h3>
     <table class="table table-bordered table-striped">
-      <thread>
+      <thead>
          <tr class="table-light-grey">
-         <th>ID</th>
-         <th>Fund Name</th>
-         <th>Ticker</th>
-         <th>Last Closing Date</th>
-         <th>Price</th>
+           <th>Fund Name</th>
+           <th>Ticker</th>
+           <th>Last Closing Date</th>
+           <th class="text-right">Price</th>
          </tr>
-      </thread>
+      </thead>
       <c:forEach var="researchFund" items="${researchFundList}">
       <tr>
-      	  <td>${fn:escapeXml(researchFund.fundId)}</td>
           <td>${fn:escapeXml(researchFund.fundName)}</td>
           <td>${fn:escapeXml(researchFund.ticker)}</td>
           <td>${fn:escapeXml(researchFund.lastClosingDate)}</td>
-          <td>${fn:escapeXml(researchFund.price)}</td>
+          <td class="text-right"><fmt:formatNumber value="${researchFund.price}" type="currency"/></td>
       </tr>
       </c:forEach>
     </table>
-    <table class="table table-bordered table-striped">
-   	 <c:forEach var="priceHistoryMap" items="${mapList}" varStatus="status">
-   	 <tr>
-   	 	<td>Date</td>
-   	 	<td>Price</td>
-   	 </tr>
-    	<c:forEach var="date" items="${dateList[status.index]}">
-    	<tr>
-    		<td>${fn:escapeXml(date)}</td>
-    		<td>${fn:escapeXml(priceHistoryMap.get(date))}</td>
-    	</tr>
-    	</c:forEach>
-    	<p></p>	
-	</c:forEach>
-	</table>
+    <h3>Price History</h3>
+    <div class="js-research-fund-chart"></div>
+    <c:forEach var="priceHistoryMap" items="${mapList}" varStatus="status">
+      <h4>${fn:escapeXml(researchFundList[status.index].fundName)}</h4>
+      <table class="table table-bordered table-striped js-fund-table" data-ticker="${fn:escapeXml(researchFundList[status.index].ticker)}">
+        <tr>
+          <th>Date</th>
+          <th class="text-right">Price</th>
+        </tr>
+        <c:forEach var="date" items="${dateList[status.index]}" varStatus="status2">
+          <tr>
+            <td class="js-fund-date" data-date-iso="${fn:escapeXml(date)}">
+              ${fn:escapeXml(dateDispList[status.index][status2.index])}
+            </td>
+            <td class="text-right js-fund-price" data-price="${fn:escapeXml(priceHistoryMap.get(date))}">
+              <fmt:formatNumber value="${priceHistoryMap.get(date)}" type="currency"/>
+            </td>
+          </tr>
+        </c:forEach>
+      </table>
+    </c:forEach>
 <h3>${fn:escapeXml(message)}</h3>
 </main>
 <%@ include file="footer.jsp" %>
--- webapp/src/cfs/controller/ResearchFundAction.java
+++ webapp/src/cfs/controller/ResearchFundAction.java
@@ -1,11 +1,14 @@
 package cfs.controller;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -40,28 +43,31 @@ public class ResearchFundAction extends Action {
                 request.setAttribute("message", "Currently we have no fund.");
                 return "research-fund.jsp";
             }
-            
+
             List<Map<String, Double>> mapList = new ArrayList<Map<String, Double>>();
             List<List<String>> dateList = new ArrayList<List<String>>();
+            List<List<String>> dateDispList = new ArrayList<List<String>>();
 
             int[] fundIdList = new int[funds.length];
             for (int i = 0; i < funds.length; i++) {
                 fundIdList[i] = funds[i].getFundId();
             }
             Arrays.sort(fundIdList);
-            
+
             for (int i = 0; i < fundIdList.length; i++) {
                 Map<String, Double> priceHistoryMap = new HashMap<String, Double>();
                 List<String> dates = new ArrayList<String>();
+                List<String> dateDisps = new ArrayList<String>();
                 FundPriceHistory[] history = fundPriceHistoryDAO.priceTrend(fundIdList[i]);
                 if (history == null || history.length == 0) {
                     priceHistoryMap.put("N/A", null);
                     dates.add("N/A");
-                    System.out.println(dates);
+                    dateDisps.add("N/A");
                 }
                 for (int j = 0; j < history.length; j++) {
                     priceHistoryMap.put(history[j].getExecuteDate(), history[j].getPrice());
                     dates.add(history[j].getExecuteDate());
+                    dateDisps.add(dateDisp(history[j].getExecuteDate()));
                 }
                 mapList.add(priceHistoryMap);
                 request.setAttribute("priceHistoryMap", priceHistoryMap);
@@ -69,9 +75,9 @@ public class ResearchFundAction extends Action {
                 Collections.sort(dates);
                 }
                 dateList.add(dates);
-                request.setAttribute("dates", dates);
+                dateDispList.add(dateDisps);
             }
-            
+
             ResearchFundView[] researchFundList = new ResearchFundView[funds.length];
             for (int i = 0; i < researchFundList.length; i++) {
                 Fund fund = fundDAO.read(fundIdList[i]);
@@ -79,13 +85,14 @@ public class ResearchFundAction extends Action {
                 String lastDate = datesOfFund.get(datesOfFund.size() -1);
                 Map<String, Double> mapOfFund = mapList.get(i);
                 Double price = mapOfFund.get(lastDate);
-                ResearchFundView researchFund = merge(fund, lastDate, price);
+                ResearchFundView researchFund = merge(fund, dateDisp(lastDate), price);
                 researchFundList[i] = researchFund;
             }
-            
+
             request.setAttribute("researchFundList", researchFundList);
             request.setAttribute("mapList", mapList);
             request.setAttribute("dateList", dateList);
+            request.setAttribute("dateDispList", dateDispList);
             System.out.println(mapList);
             return "research-fund.jsp";
         } catch (RollbackException e) {
@@ -101,9 +108,14 @@ public class ResearchFundAction extends Action {
         researchFund.setTicker(fund.getTicker());
         researchFund.setLastClosingDate(lastDate);
         researchFund.setPrice(price);
-        return researchFund;      
+        return researchFund;
     }
- 
+    private String dateDisp(String date) {
+        if (date == null) return null;
+        LocalDate date1 = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+        return (date1.format(DateTimeFormatter.ofPattern("MM/dd/yyyy", new Locale("en", "US"))));
+    }
+
     @Override
     public AccessControlLevel getAccessControlLevel() {
         return AccessControlLevel.Customer;
